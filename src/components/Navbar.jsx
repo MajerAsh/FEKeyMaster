8417b1eb637ecb7151a326ffbaa05a20e28adf6b
Ashley Majer <Majerash@gmail.com>
2025-10-07 13:57:53 -0400

    setting up athroutes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export default function Navbar() {
+  const { user, logout } = useAuth();
+
   return (
     <nav>
       <Link to="/">Play</Link> | <Link to="/create">Create Puzzle</Link> |{" "}
-      <Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
+      {!user ? (
+        <>
+          <Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
+        </>
+      ) : (
+        <>
+          <span>Welcome, {user.email}!</span>{" "}
+          <button onClick={logout}>Logout</button>
+        </>
+      )}
     </nav>
   );
 }
