302aa5a4cc094bb56406ef5642d91999a8c70c54
Ashley Majer <Majerash@gmail.com>
2025-10-21 13:24:50 -0400

    dial lock rendered!

--- src/components/DialLock.jsx
+++ src/components/DialLock.jsx
@@ -23,16 +23,25 @@ export default function DialLock({
   return (
     <div className="flex flex-col items-center">
       <h3 className="text-xl font-semibold mb-4">Dial the Combination</h3>
+
+      {/* Render dial lock background */}
+      <img src="/images/dial-lock.png" alt="Dial Lock" className="w-64 mb-4" />
+
       <div className="flex gap-6">
         {values.map((v, i) => (
-          <div
-            key={i}
-            className="w-16 h-24 relative flex flex-col items-center justify-center"
-          >
-            <div className="w-full h-full bg-gray-100 border-2 border-gray-400 rounded-lg flex flex-col items-center justify-center relative">
-              <div className="text-3xl font-mono transform rotate-0">{v}</div>
+          <div key={i} className="relative w-16 h-16">
+            {/* Dial image */}
+            <img
+              src="/images/dial.png"
+              alt={`Dial ${i + 1}`}
+              className="w-full h-full object-contain"
+            />
+            {/* Number overlay */}
+            <div className="absolute inset-0 flex items-center justify-center text-xl font-bold text-white">
+              {v}
             </div>
-            <div className="absolute -top-6 flex gap-2">
+            {/* Controls */}
+            <div className="absolute -top-6 flex gap-2 left-1/2 -translate-x-1/2">
               <button
                 onClick={() => handleChange(i, 1)}
                 className="w-6 h-6 bg-blue-500 text-white text-sm rounded-full hover:bg-blue-600"
@@ -49,6 +58,7 @@ export default function DialLock({
           </div>
         ))}
       </div>
+
       <button
         onClick={handleSubmit}
         className="mt-6 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
